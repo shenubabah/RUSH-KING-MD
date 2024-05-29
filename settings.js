2893c00e10ee86a76dbbc87980470dea345a8f51
shenubabah <dilshanrashmika215@gmail.com>
2024-05-29 10:12:18 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+global.email = 'vajirarathnayaka891@gmail.com'
+global.github = 'https://github.com/VajirTech/VAJIRA-MD'
+global.location = 'Srilanka Colombo'
+global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
+global.author = 'RUSH-KING-MD';
+global.mess = {
+    banned: '`You are *Banned* fron using commands!`',
+    bangc: '`This Group is *Banned* from using Commands!`',
+    success: '`✓ Success`',
+    admin: '`This feature is only for group admins`',
+    botAdmin: '`I am not an admin!`',
+    owner: '`You are not my owner`',
+    group: '`You can use this command only in groups ❌`',
+    private: '`Feature is used only for private chats!`',
+    bot: '`Bot number user special features`',
+    wait: '*Processing Your request*',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
+}
+
+global.owner = [process.env.OWNER_NUMBER] || ['94762498519'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+
+module.exports = {    
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=bBlhXQBI#u4kBC0saM--jADdegA5h9EtqNDbaZGQkbUTGRDBVulM',
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
+PREFIX:  process.env.PREFIX || ['.'] ,
+LANG:  process.env.LANG || 'SI' ,    
+ANTI_BAD: process.env.ANTI_BAD || false  ,
+AUTO_REACT:  process.env.AUTO_REACT  || true  ,    
+AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
+AUTO_RECORDING:  process.env.AUTO_RECORDING  || true  ,
+AUTO_READ:  process.env.AUTO_READ  || false  ,
+AUTO_BIO:  process.env.AUTO_BIO  || false  ,    
+ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,            
+WORK_TYPE: process.env.WORK_TYPE || 'public'  ,
+ANTI_LINK: process.env.ANTI_LINK || false  ,
+ANTI_BOT: process.env.ANTI_BOT || false  ,
+OWNER_NUMBER: process.env.OWNER_NUMBER || '94762498519'  ,    
+ALIVE: process.env.ALIVE || `default`,
+OWNER_NAME: process.env.OWNER_NAME || 'rush' ,     
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,    
+AUTO_VOICE:  process.env.AUTO_VOICE  || true  ,
+AUTO_STICKER: process.env.AUTO_STICKER || true  ,
+WELCOME:  process.env.WELCOME  || true ,    
+NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK || false  ,
+FOOTER: process.env.FOOTER ||  '©RUSH - ＭＤ - Ｖ4',
+LOGO: process.env.LOGO || `https://telegra.ph/file/4e7d119a85506f67f59ff.jpg` ,
+IMAGE: process.env.IMAGE || `https://telegra.ph/file/4e7d119a85506f67f59ff.jpg`     
+};
+
